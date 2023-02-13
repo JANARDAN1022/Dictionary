f4a3093586552264236f352e6adf1eff24443bc1
JD <janardanayachi3@gmail.com>
2023-02-13 22:21:41 +0530

    fixed api not working problem

--- src/components/Meaning.jsx
+++ src/components/Meaning.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 import '../index.css'
 
 const Meaning = (props) => {
-  const { word, result, setWord } = props;
+  const { word, result, setWord,setResult } = props;
+
+  const onClearClick=()=>{
+     setWord('');
+     setResult('');
+  }
 
   return (
     <div className='main'>
       <h1 className="head">ENGLISH DICTIONARY</h1>
       <input value={word} onChange={(e) => setWord(e.target.value)} onKeyDown={props.onKeyDown} className='input' type="text" placeholder='Search for a Word' />
+      <button onClick={onClearClick} className='CLEAR'>CLEAR</button>
       <div className='result'>
         <p>YOUR WORD : <span className='span'>{word}</span></p>
-        <p>MEANING : <span className='span2'>
-          {result.entries && result.entries[0] && result.entries[0].sense && result.entries[0].sense[0] 
-            ? result.entries[0].sense[0].definition 
-            : 'No definition found'
-          }
-        </span></p>
+        <p>MEANING : <span className='span2'>{result}</span></p>
       </div>
     </div>
-  )
+  );
 };
 
-
 export default Meaning;
-
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,6 +7,7 @@ const Search = ({ word, setWord, onKeyDown }) => {
       <h1 className="head">ENGLISH DICTIONARY</h1>
       <input value={word} onChange={(e) => setWord(e.target.value)} onKeyDown={onKeyDown} className='input' type="text" placeholder='Search for a Word' />
       <p>Type a Word and press enter</p>
+      <p>{"(takes some time to load the first meaning so pls wait)"}</p>
     </div>
   );
 };
