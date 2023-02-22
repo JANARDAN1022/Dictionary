e1ea584c49b9684e65241741689aeec30795c4b3
JD <janardanayachi3@gmail.com>
2023-02-22 22:49:07 +0530

    added focus functionality on input field to be user friendly

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React,{useRef,useEffect} from 'react';
 import '../index.css';
 
+
+
 const Search = ({ word, setWord, onKeyDown }) => {
+  
+  const inputRef = useRef(null);
+    
+    useEffect(()=>{
+      inputRef.current.focus();
+    },[]);
+    
+  
   return (
     <div className='main'>
       <h1 className="head">ENGLISH DICTIONARY</h1>
-      <input value={word} onChange={(e) => setWord(e.target.value)} onKeyDown={onKeyDown} className='input' type="text" placeholder='Search for a Word' />
+      <input value={word} ref={inputRef} onChange={(e) => setWord(e.target.value)} onKeyDown={onKeyDown} className='input' type="text" placeholder='Search for a Word' />
       <p>Type a Word and press enter</p>
       <p>{"(takes some time to load the first meaning so pls wait)"}</p>
     </div>
